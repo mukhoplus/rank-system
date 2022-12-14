58d3adb779fcc54df7b26c3d85c3552ab9a9885a
Seungwan <mukho@kakao.com>
2022-12-14 14:54:32 +0900

    Ver 0.2.1: 삼성 인터넷-쿠키 undefined 문제 해결

--- src/main/java/com/example/ranksystem/RankSystemApplication.java
+++ src/main/java/com/example/ranksystem/RankSystemApplication.java
@@ -15,5 +15,5 @@ public class RankSystemApplication {
 
 /* Use snapshot
 ./gradlew build
-java -jar build/libs/rank-system-0.2.0-SNAPSHOT.jar
+java -jar build/libs/rank-system-0.2.1-SNAPSHOT.jar
  */
