f5a39a438089307e94810e6cc87a8813564c1d93
Mukho <mukho@kakao.com>
2025-08-19 19:15:44 +0900

    Fix: 랭킹 소프트리셋 관련 쿼리 수정

--- src/main/java/com/mukho/ranksystem/Repository/RankingRepository.java
+++ src/main/java/com/mukho/ranksystem/Repository/RankingRepository.java
@@ -11,9 +11,9 @@ import com.mukho.ranksystem.Dto.RankDto;
 @Repository
 public interface RankingRepository extends CrudRepository<RankDto, Long> {
 
-    @Query(value = "SELECT name, race, rating, wins, loses, ROUND(100. * wins/(wins+loses), 2) AS win_rate, " +
+    @Query(value = "SELECT name, race, rating, wins, loses, ROUND(100. * wins / NULLIF(wins+loses, 0), 2) AS win_rate, " +
         "rank() OVER (ORDER BY rating DESC, 100. * wins/(wins+loses) DESC, wins DESC) AS rank_num " +
-        "FROM gamer WHERE wins+loses > 0 OR rating != 1000.00;", nativeQuery = true)
+        "FROM gamer WHERE (wins+loses > 0) OR (wins + loses = 0 AND rating != 1000);", nativeQuery = true)
     List<RankDto> getRanking();
 
 }
